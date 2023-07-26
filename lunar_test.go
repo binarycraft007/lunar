5c2fe2e545c4a0644d3c58eab9b5bd97b1fa3e36
Binary Craft <elliot.huang.signed@gmail.com>
2023-07-26 13:35:06 +0800

    test all date convertion

--- lunar_test.go
+++ lunar_test.go
@@ -1,27 +1,31 @@
 package lunar
 
 import (
-	"fmt"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
 )
 
-func TestFirstDayLookup(t *testing.T) {
+func TestAllLookup(t *testing.T) {
 	l, err := NewLunar()
 	if err != nil {
 		t.Fatalf("init lunar: %v", err)
 	}
 
 	for name, _ := range l.LookupMap {
-		year := strings.TrimSuffix(name, ".txt")
-		dateStr := fmt.Sprintf("%s-01-01", year)
-		timeIn, err := time.Parse("2006-01-02", dateStr)
-		if err != nil {
-			t.Fatalf("parse time: %v", err)
-		}
-		if _, err = l.TimeToLunar(timeIn); err != nil {
-			t.Fatalf("to lunar: %v", err)
+		yearStr := strings.TrimSuffix(name, ".txt")
+		year, err := strconv.Atoi(yearStr)
+		date := time.Date(year, time.January, 1, 0, 0, 0, 0, time.UTC)
+		for date.Year() == year {
+			t.Log("original gregorian date:", date.Format("2006-01-01"))
+			var lunarDate *string
+			if lunarDate, err = l.TimeToLunar(date); err != nil {
+				t.Fatalf("to lunar: %v", err)
+			}
+			t.Log("converted lunar date:", *lunarDate)
+
+			date = date.AddDate(0, 0, 1)
 		}
 	}
 }
